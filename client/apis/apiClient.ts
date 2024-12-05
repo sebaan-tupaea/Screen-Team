3ee138272ed5b5c2a8a4645a60944e138302bf69
Cellyane Lima <cellyane@gmail.com>
2024-12-05 14:45:41 +1300

    merge

--- client/apis/apiClient.ts
+++ client/apis/apiClient.ts
@@ -1,16 +1,16 @@
-const API_URL = 'http://localhost:3000/api/v1/movies'; // Adjust if needed I am actually unsure of this...
+const API_URL = 'http://localhost:3000/api/v1/movies' // Adjust if needed I am actually unsure of this...
 
 // Function to get all movies
 export const getAllMovies = async () => {
   try {
-    const response = await fetch(API_URL);
-    if (!response.ok) { 
-    throw new Error('Network response was not ok');
-    }  
-    const data = await response.json();
-    return data;
-    } catch (error) {
-    console.error('Error fetching movies:', error);
-    throw error;
+    const response = await fetch(API_URL)
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
     }
-};
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error('Error fetching movies:', error)
+    throw error
+  }
+}
